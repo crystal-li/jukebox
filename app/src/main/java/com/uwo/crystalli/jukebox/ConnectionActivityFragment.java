a7cb73144d3fd453d1daccfedca8418ab15a9d0f
Crystal <cl.crystal.li@gmail.com>
2016-03-02 19:28:09 -0500

    Sample to connect to local express server - changes textview in ConnectionActivity

--- app/src/main/java/com/uwo/crystalli/jukebox/ConnectionActivityFragment.java
+++ app/src/main/java/com/uwo/crystalli/jukebox/ConnectionActivityFragment.java
@@ -1,15 +1,29 @@
 package com.uwo.crystalli.jukebox;
 
+import android.os.AsyncTask;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 /**
  * A placeholder fragment containing a simple view.
  */
 public class ConnectionActivityFragment extends Fragment {
+
+    private final String LOG_TAG = ConnectionActivityFragment.class.getSimpleName();
 
     public ConnectionActivityFragment() {
     }
@@ -17,6 +31,89 @@ public class ConnectionActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+        sendURLRequestTask reqTask = new sendURLRequestTask();
+        Log.v(LOG_TAG, "Executing reqTask");
+        reqTask.execute();
+
+        //TODO: not sure if this is right, but so we can access views in OnPostExecute()
+        View rootView = inflater.inflate(R.layout.fragment_connection, container, false);
+
         return inflater.inflate(R.layout.fragment_connection, container, false);
     }
+
+
+    public class sendURLRequestTask extends AsyncTask<Void, Void, String> {
+
+        private final String LOG_TAG = sendURLRequestTask.class.getSimpleName();
+
+        @Override
+        protected String doInBackground(Void... params) {
+            // These two need to be declared outside the try/catch
+            // so that they can be closed in the finally block.
+            HttpURLConnection urlConnection = null;
+            BufferedReader reader = null;
+
+            // Will contain the raw JSON response as a string.
+            String testString = null;
+
+            try {
+
+                URL url = new URL("http://10.0.2.2:3000");
+                urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setRequestMethod("GET");
+                urlConnection.connect();
+
+                // Read the input stream into a String
+                InputStream inputStream = urlConnection.getInputStream();
+                StringBuffer buffer = new StringBuffer();
+                if (inputStream == null) {
+                    // Nothing to do.
+                    return null;
+                }
+                reader = new BufferedReader(new InputStreamReader(inputStream));
+
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    // Since it's JSON, adding a newline isn't necessary (it won't affect parsing)
+                    // But it does make debugging a *lot* easier if you print out the completed
+                    // buffer for debugging.
+                    buffer.append(line + "\n");
+                }
+
+                if (buffer.length() == 0) {
+                    // Stream was empty.  No point in parsing.
+                    return null;
+                }
+                testString = buffer.toString();
+            } catch (IOException e) {
+                Log.e(LOG_TAG, "Error ", e);
+                // If the code didn't successfully get the weather data, there's no point in attemping
+                // to parse it.
+                return null;
+            } finally {
+                if (urlConnection != null) {
+                    urlConnection.disconnect();
+                }
+                if (reader != null) {
+                    try {
+                        reader.close();
+                    } catch (final IOException e) {
+                        Log.e(LOG_TAG, "Error closing stream", e);
+                    }
+                }
+            }
+            Log.v(LOG_TAG, testString);
+            return testString;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+
+            //set textView as the response
+            TextView textView = (TextView) getActivity().findViewById(R.id.test_string_textview);
+            textView.setText(result);
+        }
+    }
 }
+
