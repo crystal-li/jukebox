65e91e97c5f3c71812f343e8db541c34da912445
Crystal <cl.crystal.li@gmail.com>
2016-03-08 00:37:58 -0500

    AddMedia searches youtube and displays results in listview

---
+++ app/src/main/java/com/uwo/crystalli/jukebox/VideoResult.java
@@ -0,0 +1,30 @@
+package com.uwo.crystalli.jukebox;
+
+public class VideoResult {
+
+    private String id;
+    private String title;
+    private String thumbUrl; //All seem to be 120 x 90 for the default
+
+    //TODO: don't know what this is
+    public VideoResult() {
+        super();
+    }
+
+    public VideoResult(String id, String title, String thumbUrl) {
+        super();
+        this.id = id;
+        this.title = title;
+        this.thumbUrl = thumbUrl;
+    }
+
+    public String getId() {
+        return this.id;
+    }
+
+    @Override
+    //TODO: Not sure if we need this
+    public String toString() {
+        return this.title;
+    }
+}
